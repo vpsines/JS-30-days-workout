048150d92d1ca897a1a999d2b6673d07626cf148
vpsines <sasip6028@gmail.com>
2023-03-19 07:06:50 +0530

    excercise : day1

---
+++ day1-introduction/excercises/datatypes.js
@@ -0,0 +1,53 @@
+let fruit = "Apple";
+let ageInYears = 22;
+let isSmiling = false
+const PI =3.14
+let unknown
+let empty = null
+
+console.log(typeof fruit)
+console.log(typeof ageInYears)
+console.log(typeof isSmiling)
+console.log(typeof PI)
+console.log(typeof unknown)
+console.log(typeof empty)
+
+
+// declare four variable without value
+
+let fullName
+let school
+let city
+let county
+console.log(fullName,school,city,county)
+
+// declare 4 varibles with assigned values
+
+let language = "JS"
+let courseName = "30-DAYS-OF-JS"
+let duration = 30
+let isVsCodeEditor = true
+
+console.log(language,courseName,duration,isVsCodeEditor)
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName
+let lastName
+let maritalStatus
+let country
+let age
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let fName = "Vyshnav", lName="Sasidharan",maritalStatus1 = false, country1 = "India", age1 = 22
+
+console.log(fName,lName,maritalStatus1,country1,age1)
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge =30
+let yourAge = 23
+console.log("I am ",myAge," years old.")
+console.log("You are ",yourAge," years old.")
+ 
---
+++ day1-introduction/excercises/variables.js
@@ -0,0 +1,6 @@
+let myName = 'vpsines'
+let isIndian = true
+let dateOfBirth
+let isWorking = null
+
+console.log(myName,dateOfBirth,isIndian,isWorking)
