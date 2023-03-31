e5804a80b91e15627657c63fbd8103cf323fa88f
vpsines <sasip6028@gmail.com>
2023-03-31 10:01:46 +0530

    exercise : day 13 level2

---
+++ day13-console-object-methods/exercises/level2.js
@@ -0,0 +1,8 @@
+// 10 > 2 * 10 use console.assert()
+console.assert(10 > 2*10,'10 is not greater')
+
+// Write a warning message using console.warn()
+console.warn('This is a warning!')
+
+// Write an error message using console.error()
+console.error('This is an error message... error 404')
