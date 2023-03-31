8a412b498286e58b810224df3161ae8a880ab8ed
vpsines <sasip6028@gmail.com>
2023-03-31 10:01:36 +0530

    exercise : day 13 level1

---
+++ day13-console-object-methods/exercises/level1.js
@@ -0,0 +1,21 @@
+// console.log()
+// console.warn()
+// console.error()
+// console.time()
+// console.assert()
+// console.group()
+// console.table()
+// console.clear()
+// console.count()
+// console.info()
+
+// Display the countries array as a table
+console.table(countries)
+
+// Display the countries object as a table
+console.table(countries[0])
+
+// Use console.group() to group logs
+console.group('Countries list')
+console.log(countries)
+console.groupEnd()
