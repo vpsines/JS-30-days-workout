5ce3987d6ddfbb85980acfdd4820272daceb72e8
vpsines <sasip6028@gmail.com>
2023-04-02 21:09:18 +0530

    exercise : day17 level1

---
+++ day17-web-storages/exercises/level1.js
@@ -0,0 +1,17 @@
+// Store you first name, last name, age, country, city in your browser localStorage.
+console.log(localStorage)
+
+localStorage.setItem("firstName","Vyshnav")
+console.log(localStorage)
+
+localStorage.setItem("lastName","Sasidharan")
+console.log(localStorage)
+
+localStorage.setItem("age",23)
+console.log(localStorage)
+
+localStorage.setItem("country","India")
+console.log(localStorage)
+
+localStorage.setItem("city","Kochi")
+console.log(localStorage)
