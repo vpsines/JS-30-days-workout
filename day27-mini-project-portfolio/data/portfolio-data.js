c3d85068d34da1a61442f5cac8bebc79f3e9a36e
vpsines <sasip6028@gmail.com>
2023-04-14 14:34:27 +0530

    exercise : day27 data

---
+++ day27-mini-project-portfolio/data/portfolio-data.js
@@ -0,0 +1,50 @@
+const data = {
+      firstName: 'Vyshnav',
+      lastName: 'Sasidharan',
+      titles: [
+        ['🌱', 'B.Tech In CSE'],
+        ['💻', 'Programmer'],
+        ['🌐', 'Developer'],
+        ['🔥', 'Gamer'],
+        ['📔', 'Anime Lover']
+      ],
+      qualifications: [
+        'MSc. Computer Science Ongoing',
+        'BSc. Information and Communication Eng.',
+        'MSc. Food Technology',
+        'BSc.Food Technology'
+      ],
+      socialLinks: [
+        {
+          social: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/asabeneh/',
+          fontawesomeIcon: '<i class="fab fa-linkedin">'
+        },
+        {
+          social: 'Leetcode',
+          url: 'https://twitter.com/Asabeneh',
+          fontawesomeIcon: '<i class="fab fa-twitter-square"></i>'
+        },
+        {
+          social: 'Github',
+          fontawesomeIcon: '<i class="fab fa-github-square"></i>',
+          url: 'https://github.com/Asabeneh'
+        },
+        {
+          social: 'Instagram',
+          fontawesomeIcon: '',
+          url: 'https://dev.to/asabeneh'
+        }
+      ],
+      skills: [
+        'Flutter',
+        'Node.js',
+        'JavaScript',
+        'Java',
+        'SQL',
+        'Angular',
+        '. Net'
+      ],
+      bio:
+        '<p>A Software Engineer with 3 years of experience in developing mobile applications using <b>Flutter, Firebase, Node.js, SQL, Java, and Kotlin</b>. Had successfully developed projects such as <b>BrainStormMania, Nattupedika, GithubBoards, Amazone-clone, Tic-Tac-Toe,</b> and <b>E-VCharge</b>. Had remote experience in working as a Flutter Developer at Finnkassa Oy, where I had developed and implemented an ecommerce mobile application, conceptualized a cross-platform POS android application, and formulated a web application. Currently, I am working as a <b>Software Engineer at Experion Technologies</b>. My education background is a <b>Bachelor\'s degree in Computer Science Engineering</b> from TocH Institute of Science & Technology, with a CGPA of 8.04. I had experience in adopting project life cycle methodologies, has excellent collaboration skills, and is highly experienced in building high-quality mobile applications.</p>'
+}
